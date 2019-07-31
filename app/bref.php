24e06771bdd3d1ec48dcac482c060ca086f6a501
Tatthep <tatthep.c@hoteliers.guru>
2019-07-31 16:51:15 +0700

    feat(slim-framework): init for slim framework

--- app/bref.php
+++ app/bref.php
@@ -1,9 +1,28 @@
 <?php
 
+use Bref\Bridge\Slim\SlimAdapter;
+
 require __DIR__.'/vendor/autoload.php';
 
-λ(function (array $event) {
-    return [
-        'hello' => $event['name'] ?? 'world',
-    ];
+define('API_VERSION', 'latest');
+
+$slim = new Slim\App;
+
+$slim->get(ApiPath(), function ($request, $response) {
+    $response->getBody()->write('Hello world');
+    return $response;
 });
+
+$slim->get(ApiPath('dev'), function ($request, $response) {
+    $response->getBody()->write('Hello world Dev!');
+    return $response;
+});
+
+$app = new \Bref\Application;
+$app->httpHandler(new SlimAdapter($slim));
+$app->run();
+
+function ApiPath($path = '')
+{
+    return sprintf('/%s/%s', API_VERSION, $path);
+}
